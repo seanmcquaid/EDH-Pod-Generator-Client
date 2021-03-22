0ceee15c4da2ab9d4d794951f9fff5ad00438154
seanmcquaid <sean.mcquaid2@verizon.net>
2021-03-22 19:49:59 -0400

    finished text input component

--- src/components/TextInput.vue
+++ src/components/TextInput.vue
@@ -1,10 +1,26 @@
 <template>
-  <label></label>
+  <label>
+    {{ label }}
+    <input type="text" @change="onChange" :value="value" />
+  </label>
 </template>
 
 <script>
 export default {
-  props: {},
+  props: {
+    label: {
+      type: String,
+      required: false,
+    },
+    onChange: {
+      type: Function,
+      required: true,
+    },
+    value: {
+      type: String,
+      required: false,
+    },
+  },
 };
 </script>
 
