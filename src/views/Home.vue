3aa76009b32bc0ee9394a587742dabcaefd42b75
seanmcquaid <sean.mcquaid2@verizon.net>
2021-03-23 19:32:52 -0400

    set up my basic nav component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,15 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <PageLayout></PageLayout>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import PageLayout from '@/layouts/PageLayout.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+    PageLayout,
   },
 };
 </script>
