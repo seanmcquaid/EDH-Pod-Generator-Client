1ae5dda3ab5cc2e1a670d5408d550e15e8032239
seanmcquaid <sean.mcquaid2@verizon.net>
2021-03-30 18:05:25 -0400

    wrote out test scenarios for e2e test for register page

---
+++ tests/e2e/specs/register.spec.js
@@ -0,0 +1,7 @@
+describe('Register page', () => {
+  it("Display error when passwords don't match", () => {});
+
+  it("User will see error when their user info isn't valid", () => {});
+
+  it('Successfully redirects a user when provided with valid registration info', () => {});
+});
