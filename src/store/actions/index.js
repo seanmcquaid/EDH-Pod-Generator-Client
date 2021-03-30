3e800ac3eec0b709448efc095464d3a7d72cb3b6
seanmcquaid <sean.mcquaid2@verizon.net>
2021-03-30 17:59:37 -0400

    finished store tests

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -9,14 +9,15 @@ import {
 const actions = {
   register: ({ commit }, { username, password }) => {
     commit(SET_LOADING);
-    axios(`${process.env.VUE_APP_API_URL}/users/register`, {
-      username,
-      password,
-    })
+    return axios
+      .post(`${process.env.VUE_APP_API_URL}/users/register`, {
+        username,
+        password,
+      })
       .then(({ data }) => {
         const { token } = data;
         commit(AUTH_SUCCESS, { token });
-        return Promise.resolve();
+        return Promise.resolve({ token });
       })
       .catch((err) => {
         console.log(err);
