f68d02a6bc91b6f3abd54fdb75ea1b076d950e6a
seanmcquaid <sean.mcquaid2@verizon.net>
2021-03-29 21:37:26 -0400

    set up register action

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,5 +1,32 @@
+import axios from 'axios';
+import {
+  AUTH_SUCCESS,
+  SET_ERROR_MESSAGE,
+  SET_LOADING,
+  SET_NOT_LOADING,
+} from '../mutations/types';
+
 const actions = {
-  register: ({ commit, state }, { username, password }) => {},
+  register: ({ commit }, { username, password }) => {
+    commit(SET_LOADING);
+    axios(`${process.env.VUE_APP_API_URL}/users/register`, {
+      username,
+      password,
+    })
+      .then(({ data }) => {
+        const { token } = data;
+        commit(AUTH_SUCCESS, { token });
+        return Promise.resolve();
+      })
+      .catch((err) => {
+        console.log(err);
+        commit(SET_ERROR_MESSAGE, err);
+        return Promise.reject();
+      })
+      .finally(() => {
+        commit(SET_NOT_LOADING);
+      });
+  },
 };
 
 export default actions;
