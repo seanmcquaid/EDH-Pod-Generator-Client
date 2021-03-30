88dcf044630cb420a8f6e4d4f069c4f11270a154
seanmcquaid <sean.mcquaid2@verizon.net>
2021-03-29 21:02:41 -0400

    added getters

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,3 +1,13 @@
-const getters = {};
+const getters = {
+  isAuthenticated: (state) => {
+    return state?.isAuthenticated ?? false;
+  },
+  token: (state) => {
+    return state?.token ?? null;
+  },
+  pods: (state) => {
+    return state?.pods ?? [];
+  },
+};
 
 export default getters;
---
+++ src/views/Register.vue
@@ -0,0 +1,12 @@
+<template>
+  <PageLayout></PageLayout>
+</template>
+
+<script>
+import PageLayout from '../layouts/PageLayout.vue';
+export default {
+  components: { PageLayout },
+};
+</script>
+
+<style></style>
