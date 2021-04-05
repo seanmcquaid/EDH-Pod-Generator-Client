603ef6c48c92144a591e7621984bd265e6fac4d3
seanmcquaid <sean.mcquaid2@verizon.net>
2021-04-05 18:15:15 -0400

    set up logic for confirming the pod name

--- src/views/AddPod/AddPod.vue
+++ src/views/AddPod/AddPod.vue
@@ -1,16 +1,52 @@
 <template>
   <PageLayout>
     <header><H1>Add Pod</H1></header>
-    <main></main>
+    <main>
+      <div v-if="!isNameConfirmed">
+        <TextInput
+          type="text"
+          :onChange="podNameOnChange"
+          :value="podName"
+          name="podName"
+          label="Pod Name"
+        />
+        <Button type="button" :onClick="confirmNameButtonOnClick">
+          Confirm Name
+        </Button>
+      </div>
+      <div v-else></div>
+    </main>
   </PageLayout>
 </template>
 
 <script>
+import { reactive, toRefs } from '@vue/reactivity';
 import H1 from '../../components/Typography/H1.vue';
 import PageLayout from '../../layouts/PageLayout.vue';
+import TextInput from '../../components/TextInput.vue';
+import Button from '../../components/Button.vue';
 export default {
-  components: { PageLayout, H1 },
-  setup() {},
+  components: { PageLayout, H1, TextInput, Button },
+  setup() {
+    const state = reactive({
+      podName: '',
+      isNameConfirmed: false,
+    });
+
+    const podNameOnChange = (event) => {
+      state[event.target.name] = event.target.value;
+    };
+
+    const confirmNameButtonOnClick = () => {
+      state.isNameConfirmed = true;
+    };
+
+    return {
+      ...toRefs(state),
+      podNameOnChange,
+      confirmNameButtonOnClick,
+    };
+  },
 };
 </script>
 
