9db8a9c09090cf00d9ed117e2774e4bedbb24bc7
seanmcquaid <sean.mcquaid2@verizon.net>
2021-04-07 19:38:02 -0400

    restructured to eliminate edge case

---
+++ src/views/PodInfo/PodInfo.vue
@@ -0,0 +1,19 @@
+<template>
+  <PageLayout>
+    <header><H1>Pod Info</H1></header>
+  </PageLayout>
+</template>
+
+<script>
+import PageLayout from '../../layouts/PageLayout.vue';
+import H1 from '../../components/Typography/H1.vue';
+
+export default {
+  components: {
+    PageLayout,
+    H1,
+  },
+};
+</script>
+
+<style></style>
