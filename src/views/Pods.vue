2398f6f033d1a6ec0c015c1db198d3e80f8b60b7
seanmcquaid <sean.mcquaid2@verizon.net>
2021-04-11 20:56:15 -0400

    added pods page to router and sketched out the page

---
+++ src/views/Pods.vue
@@ -0,0 +1,25 @@
+<template>
+  <PageLayout>
+    <header><H1>Pods</H1></header>
+    <main>
+      <ul>
+        <li>
+          <span></span>
+          <LinkButton route="">Link</LinkButton>
+        </li>
+      </ul>
+    </main>
+  </PageLayout>
+</template>
+
+<script>
+import LinkButton from '../components/LinkButton.vue';
+import H1 from '../components/Typography/H1.vue';
+import PageLayout from '../layouts/PageLayout.vue';
+export default {
+  components: { PageLayout, H1, LinkButton },
+  setup() {},
+};
+</script>
+
+<style></style>
