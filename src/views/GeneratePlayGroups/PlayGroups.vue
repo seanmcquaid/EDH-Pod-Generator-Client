324597a5dc6c783f1e7b02f4585e6180826d9106
seanmcquaid <sean.mcquaid2@verizon.net>
2021-04-14 18:20:43 -0400

    added in solution for having a scalable number of text inputs

--- src/views/GeneratePlayGroups/PlayGroups.vue
+++ src/views/GeneratePlayGroups/PlayGroups.vue
@@ -1,6 +1,7 @@
 <template>
   <ul>
     <li v-for="(playGroup, index) of playGroups" :key="index">
+      <span>Play Group #{{ index + 1 }}</span>
       <ul>
         <li
           v-for="playGroupMember of playGroup.playGroupMembers"
@@ -12,7 +13,13 @@
     </li>
   </ul>
   <div v-for="index in playGroups.length" :key="index">
-    <TextInput />
+    <TextInput
+      type="text"
+      :onChange="onChange"
+      :value="textInputsValues[index]"
+      :name="`textInput${index}`"
+      :label="`Spell Table Url For Play Group #${index + 1}`"
+    />
   </div>
 </template>
 
@@ -20,17 +27,44 @@
 import { reactive, toRefs } from '@vue/reactivity';
 import { onMounted } from '@vue/runtime-core';
 import TextInput from '../../components/TextInput.vue';
+import axios from 'axios';
+import { useStore } from 'vuex';
+import { useRouter } from 'vue-router';
 export default {
   components: { TextInput },
   setup() {
+    const store = useStore();
     const state = reactive({
       playGroups: [],
     });
+    const textInputsValues = reactive({});
+    const router = useRouter();
+    const podName = router.currentRoute.value.params.name;
 
-    onMounted(() => {});
+    onMounted(() => {
+      const config = {
+        headers: {
+          Authorization: store.state.token,
+        },
+      };
+      axios
+        .get(`${process.env.VUE_APP_API_URL}/pods/generate/${podName}`, config)
+        .then(({ data }) => {
+          state.playGroups = data.playGroups;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    });
+
+    const onChange = (event) => {
+      textInputsValues[event.target.name] = event.target.value;
+    };
 
     return {
       ...toRefs(state),
+      textInputsValues,
+      onChange,
     };
   },
 };
