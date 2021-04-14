cdbfc8d0c4f07c9a5df73a9cf7ab5f33c8003c3b
seanmcquaid <sean.mcquaid2@verizon.net>
2021-04-13 21:06:12 -0400

    started structuring play groups component

--- src/views/GeneratePlayGroups/PlayGroups.vue
+++ src/views/GeneratePlayGroups/PlayGroups.vue
@@ -1,9 +1,39 @@
 <template>
-  <ul></ul>
+  <ul>
+    <li v-for="(playGroup, index) of playGroups" :key="index">
+      <ul>
+        <li
+          v-for="playGroupMember of playGroup.playGroupMembers"
+          :key="playGroupMember.id"
+        >
+          {{ playGroupMember.member }}
+        </li>
+      </ul>
+    </li>
+  </ul>
+  <div v-for="index in playGroups.length" :key="index">
+    <TextInput />
+  </div>
 </template>
 
 <script>
-export default {};
+import { reactive, toRefs } from '@vue/reactivity';
+import { onMounted } from '@vue/runtime-core';
+import TextInput from '../../components/TextInput.vue';
+export default {
+  components: { TextInput },
+  setup() {
+    const state = reactive({
+      playGroups: [],
+    });
+
+    onMounted(() => {});
+
+    return {
+      ...toRefs(state),
+    };
+  },
+};
 </script>
 
 <style></style>
