a48a5ce4c38a4f3d3a37ec873cfff069b681354f
seanmcquaid <sean.mcquaid2@verizon.net>
2021-04-14 18:36:02 -0400

    finished the generate play groups page, need to work on E2E tests

--- src/views/GeneratePlayGroups/PlayGroups.vue
+++ src/views/GeneratePlayGroups/PlayGroups.vue
@@ -1,4 +1,5 @@
 <template>
+  <span>{{ errorMessage }}</span>
   <ul>
     <li v-for="(playGroup, index) of playGroups" :key="index">
       <span>Play Group #{{ index + 1 }}</span>
@@ -12,15 +13,18 @@
       </ul>
     </li>
   </ul>
-  <div v-for="index in playGroups.length" :key="index">
+  <form @submit.prevent="onSubmit">
     <TextInput
+      v-for="index in playGroups.length"
+      :key="index"
       type="text"
       :onChange="onChange"
-      :value="textInputsValues[index]"
+      :value="spellTableUrls[index]"
       :name="`textInput${index}`"
       :label="`Spell Table Url For Play Group #${index + 1}`"
     />
-  </div>
+    <Button type="submit">Contact Pods</Button>
+  </form>
 </template>
 
 <script>
@@ -30,14 +34,16 @@ import TextInput from '../../components/TextInput.vue';
 import axios from 'axios';
 import { useStore } from 'vuex';
 import { useRouter } from 'vue-router';
+import Button from '../../components/Button.vue';
 export default {
-  components: { TextInput },
+  components: { TextInput, Button },
   setup() {
     const store = useStore();
     const state = reactive({
       playGroups: [],
+      errorMessage: '',
     });
-    const textInputsValues = reactive({});
+    const spellTableUrls = reactive({});
     const router = useRouter();
     const podName = router.currentRoute.value.params.name;
 
@@ -58,13 +64,55 @@ export default {
     });
 
     const onChange = (event) => {
-      textInputsValues[event.target.name] = event.target.value;
+      spellTableUrls[event.target.name] = event.target.value;
+    };
+
+    const onSubmit = () => {
+      if (!areUrlsAllValid()) {
+        state.errorMessage =
+          'Please enter a url for each spell table url input!';
+        return;
+      }
+      const body = {
+        spellTableUrls,
+        playGroups: state.playGroups,
+      };
+      const config = {
+        headers: {
+          Authorization: store.state.token,
+        },
+      };
+      axios
+        .post(`${process.env.VUE_APP_API_URL}/pods/contact`, body, config)
+        .then(() => {
+          router.push('/userHome');
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+
+    const areUrlsAllValid = () => {
+      const keys = Object.keys(spellTableUrls);
+
+      if (keys.length === 0) {
+        return false;
+      }
+
+      for (let i = 0; i < keys.length; i++) {
+        if (spellTableUrls[keys[i]].length === 0) {
+          return false;
+        }
+      }
+
+      return true;
     };
 
     return {
       ...toRefs(state),
-      textInputsValues,
+      spellTableUrls,
       onChange,
+      onSubmit,
     };
   },
 };
