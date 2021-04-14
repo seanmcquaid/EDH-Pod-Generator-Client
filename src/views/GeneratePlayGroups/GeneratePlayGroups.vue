83c9cda2e423407b2999562fa8c4281ddf134d30
seanmcquaid <sean.mcquaid2@verizon.net>
2021-04-13 20:27:16 -0400

    structured page

---
+++ src/views/GeneratePlayGroups/GeneratePlayGroups.vue
@@ -0,0 +1,19 @@
+<template>
+  <PageLayout>
+    <header><H1>Generate Pods</H1></header>
+    <main>
+      <PlayGroups />
+    </main>
+  </PageLayout>
+</template>
+
+<script>
+import H1 from '../../components/Typography/H1.vue';
+import PageLayout from '../../layouts/PageLayout.vue';
+import PlayGroups from './PlayGroups.vue';
+export default {
+  components: { PageLayout, H1, PlayGroups },
+};
+</script>
+
+<style></style>
